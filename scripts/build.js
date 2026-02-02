ffdefdffe8c3d769763b6236bd15790a09c7ae6b
Alan Lima <alanreislm@gmail.com>
2026-02-02 05:11:45 -0300

    feat: add favicon and logo

--- scripts/build.js
+++ scripts/build.js
@@ -69,10 +69,7 @@ if (!fs.existsSync(publicDist)) fs.mkdirSync(publicDist, { recursive: true });
 const files = fs.readdirSync(publicSrc);
 for (const file of files) {
   if (path.extname(file) === '.html') {
-    fs.copyFileSync(
-      path.join(publicSrc, file),
-      path.join(publicDist, file)
-    );
+    fs.copyFileSync(path.join(publicSrc, file), path.join(publicDist, file));
   }
 }
 
@@ -81,9 +78,9 @@ if (fs.existsSync(path.join(publicSrc, 'css'))) {
   copyDir(path.join(publicSrc, 'css'), path.join(publicDist, 'css'));
 }
 
-// js/libs folder (Static JS libs)
-if (fs.existsSync(path.join(publicSrc, 'js/libs'))) {
-  copyDir(path.join(publicSrc, 'js/libs'), path.join(publicJsDist, 'libs'));
+// assets folder
+if (fs.existsSync(path.join(publicSrc, 'assets'))) {
+  copyDir(path.join(publicSrc, 'assets'), path.join(publicDist, 'assets'));
 }
 
 // Component CSS files (from public/ts/components/**/*.css -> dist/public/js/components/**/*.css)
